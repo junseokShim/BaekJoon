d921de107db3beb0e696d13084f2225c8b2831a8
junseokShim <shimjunseok@simjunseog-ui-MacBookPro-2.local>
2023-08-28 22:04:39 +0900

    beakjoon_1339_gridy

---
+++ success/1339.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+N = int(input())
+words = [input() for _ in range(N)]
+
+num = 9
+sum_num = 0
+alpha_weight_dict = {}
+alpha_to_num_dict = {}
+
+for word in words:
+    for i in range(len(word)):
+        if word[i] not in alpha_weight_dict:
+            alpha_weight_dict[word[i]] = 10**(len(word)-1-i)
+
+        else:
+            alpha_weight_dict[word[i]] = alpha_weight_dict[word[i]] + 10**(len(word)-1-i)
+
+
+alpha_weight_dict = sorted(alpha_weight_dict.items(), key = lambda item: item[1], reverse=True)
+
+for key in alpha_weight_dict:
+    alpha_to_num_dict[key[0]] = num
+    num -= 1
+
+print(alpha_to_num_dict)
+
+for word in words:
+    s = ''
+    for i in range(len(word)):
+        s = s + str(alpha_to_num_dict[word[i]])
+
+    sum_num += int(s)
+
+print(sum_num)
