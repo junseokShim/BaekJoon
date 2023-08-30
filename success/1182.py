5ff43a2a9455a8ad26004fc57bd04a7aec6def95
junseokShim <shimjunseok@simjunseog-ui-MacBookPro-2.local>
2023-08-30 19:35:25 +0900

    beakjoon_1182_combination

---
+++ success/1182.py
@@ -0,0 +1,34 @@
+N, S = map(int, input().split(" "))
+lst = list(map(int, input().split(" ")))
+
+cnt = 0
+visited = [0 for _ in range(N)]
+
+r= 3
+
+def dfs(idx, arr):
+    global cnt
+    if len(arr) == r:
+        if sum(arr)==S: cnt+=1
+        return arr
+
+    for i in range(idx+1, len(lst)):
+        if visited[i] == 1: continue
+        visited[i] = 1
+        dfs(i, arr + [lst[i]])
+        visited[i] = 0
+
+for r in range(1, N+1):
+    dfs(-1, [])
+
+# for r in range(1, N+1):
+
+#     r = 1
+#     result = [0 for _ in range(r)]
+#     print(result)
+#     dfs(0, [])
+
+print(cnt)
+
+
+    
